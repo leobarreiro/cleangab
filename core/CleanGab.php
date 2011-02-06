14794c6bf6dd2597ada5b69ddcc06782e3b3d4b0
Leopoldo Barreiro <leo@cielnews.com>
2011-02-06 12:31:52 -0200

    utilização de print_r para debug.

--- core/CleanGab.php
+++ core/CleanGab.php
@@ -5,7 +5,7 @@ class CleanGab {
 	static function debug($mixedVar) {
 		if (ini_get("error_reporting") > 0) {
 			echo "<pre>\n";
-			var_dump($mixedVar);
+			print_r($mixedVar);
 			echo "</pre>\n";
 		}
 	}
