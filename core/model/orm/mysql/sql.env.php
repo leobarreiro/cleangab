d47c57a34106d357a9c2ade9034d01df61701982
leobarreiro <leo@cielnews.com>
2011-05-24 16:24:54 -0300

    Adicionado SQL para recuperação de dados de uma tabela.

--- core/model/orm/mysql/sql.env.php
+++ core/model/orm/mysql/sql.env.php
@@ -1,5 +1,7 @@
 <?php
+define ("CLEANGAB_SQL_RETRIEVE_ALL", "SELECT [listable_fields] FROM `[database]`.`[table]`");
 define ("CLEANGAB_SQL_RETRIEVE_TABLE_FIELDS", "SHOW COLUMNS FROM `[database]`.`[table]` ");
+define ("CLEANGAB_SQL_RETRIEVE_USERS", "SELECT * FROM `[database]`.usuario WHERE id = [user_id] ");
 define ("CLEANGAB_SQL_RETRIEVE_PERMISSIONS", "SELECT * FROM `[database]`.permission WHERE user_id = [user_id] ");
 define ("CLEANGAB_SQL_INSERT", "INSERT INTO `[database`.`[table]` ([insert_fields]) VALUES ([insert_values])");
 define ("CLEANGAB_SQL_UPDATE", "UPDATE `[database]`.`[table]` [update_values] WHERE [update_conditions] ");
