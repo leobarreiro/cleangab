4db2242928fa45a4085f5f9a5f8c8305c168ae75
leobarreiro <leo@cielnews.com>
2011-05-24 16:22:21 -0300

    Incluídas constantes como valores padrão de parâmetros.

--- core/model/orm/mysql/Connection.php
+++ core/model/orm/mysql/Connection.php
@@ -6,9 +6,10 @@ class Connection implements IDBConnection {
 
 	public $resource;
 	
-	public function connect($host, $port, $database, $user, $pwd)
+	public function connect($host=CLEANGAB_DB_HOST, $port=CLEANGAB_DB_PORT, 
+								$database=CLEANGAB_DB_DB, $user=CLEANGAB_DB_USER, $pwd=CLEANGAB_DB_PWD)
 	{
-		$this->resource = new mysqli($host, $user, $pwd, $database, $port);
+		$this->resource = new mysqli($host, $user, $pwd, $database);
 	}
 	
 	public function __construct($host=CLEANGAB_DB_HOST, $port=CLEANGAB_DB_PORT, 
