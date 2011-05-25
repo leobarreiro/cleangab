a1e8d98d7e18b4af136641c5a57976000a3db24b
Leopoldo Barreiro <leo@cielnews.com>
2011-05-25 17:25:24 -0300

    Suporte a classe CSS nomeada de acordo com a classe do objeto.

--- core/view/TableList.php
+++ core/view/TableList.php
@@ -23,25 +23,17 @@ class TableList implements XHTMLComponent {
 	public function ensamble() {
 		
 		$content = $this->content->getRecords();
-		$xhtml  = "\n\t<table id='" . $this->idName . "' border='1'>\n";
-		
+		$xhtml  = "\n\t<table id='" . $this->idName . "' class='" . strtolower(get_class($this)) . "'>\n";
 		if (is_array($this->header) && count($this->header) > 0) {
-			
 			$xhtml .= "\t\t<tr>\n";
-			
 			foreach ($this->header as $headCell) {
 				$xhtml .= "\t\t\t<th>" . $headCell . "</th>\n";
 			}
-			
 			$xhtml .= "\t\t</tr>\n";
 			$headers = array_keys($this->header);
-			
 			foreach ($content as $record) {
-				
 				$xhtml .= "\t\t<tr>\n";
-				
 				foreach ($headers as $headCell) {
-					
 					if (is_array($this->masks) && isset($this->masks[$headCell])) {
 						$mask = $this->masks[$headCell];
 						$formatter = new $mask();
@@ -51,30 +43,21 @@ class TableList implements XHTMLComponent {
 					} else {
 						$xhtml .= "\t\t\t<td>" . $record[$headCell] . "</td>\n";
 					}
-					
 				}
-				
 				$xhtml .= "\t\t</tr>\n";
-				
 			}
-			
 		} 
 		else {
-			
 			foreach ($content as $record) {
 				$xhtml .= "\t\t<tr>\n";
-				
 				foreach ($record as $cell) {
 					$xhtml .= "\t\t\t<td>" . $cell . "</td>\n";
 				}
-				
 				$xhtml .= "\t\t</tr>\n";
 			}
-					
 		}
 		$xhtml .= "\t</table>\n\n";
 		$this->xhtml = $xhtml;
-		
 	}
 	
 	public function toXhtml() {
