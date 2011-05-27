331b75785617579cb6c8bc7a1fafea2d53855952
Leopoldo Barreiro <leo@cielnews.com>
2011-05-27 08:47:01 -0300

    Alterada query para verificação de login.

--- core/model/orm/mysql/sql.env.php
+++ core/model/orm/mysql/sql.env.php
@@ -5,6 +5,6 @@ define ("CLEANGAB_SQL_RETRIEVE_USERS", "SELECT * FROM `[database]`.usuario WHERE
 define ("CLEANGAB_SQL_RETRIEVE_PERMISSIONS", "SELECT * FROM `[database]`.permission WHERE user_id = [user_id] ");
 define ("CLEANGAB_SQL_INSERT", "INSERT INTO `[database`.`[table]` ([insert_fields]) VALUES ([insert_values])");
 define ("CLEANGAB_SQL_UPDATE", "UPDATE `[database]`.`[table]` [update_values] WHERE [update_conditions] ");
-define ("CLEANGAB_SQL_VERIFY_LOGIN", "SELECT id, nome, email FROM `[database]`.`[table]` WHERE usuario = '[username]' AND senha = MD5('[password]') ");
+define ("CLEANGAB_SQL_VERIFY_LOGIN", "SELECT id, usuario AS user, nome AS name, email FROM `[database]`.`[table]` WHERE usuario = '[username]' AND senha = MD5('[password]') ");
 
 ?>
