f539625c60f91c41a41e8e7befdabb253fe18b13
Leopoldo Barreiro <leo@cielnews.com>
2011-05-31 20:25:45 -0300

    Alteradas rotas para trabalho em estação linux. Inclusão de localconfig da aplicação no próprio config core do cleangab.

--- core/config.php
+++ core/config.php
@@ -7,8 +7,8 @@
  */
 // Constants
 
-define ("SEPARATOR", "\\");
-define ("CLEANGAB_FWK_ROOT", "C:\\Disys\\apps\\xampp\\apache\\htdocs\\cleangab\\core\\");
+define ("SEPARATOR", "/");
+define ("CLEANGAB_FWK_ROOT", "/var/www/cleangab/core/");
 define ("CLEANGAB_FWK_MODEL", CLEANGAB_FWK_ROOT . "model" . SEPARATOR);
 define ("CLEANGAB_FWK_ORM", CLEANGAB_FWK_MODEL . "orm" . SEPARATOR);
 define ("CLEANGAB_FWK_CONTROL", CLEANGAB_FWK_ROOT . "controller" . SEPARATOR);
@@ -16,24 +16,11 @@ define ("CLEANGAB_FWK_VIEW", CLEANGAB_FWK_ROOT . "view" . SEPARATOR);
 define ("CLEANGAB_FWK_SECURITY", CLEANGAB_FWK_ROOT . "security" . SEPARATOR);
 define ("CLEANGAB_FWK_FORMATTER", CLEANGAB_FWK_VIEW . SEPARATOR . "formatters" . SEPARATOR);
 
-define ("CLEANGAB_DB_DRIVER", "mysql");
-
 define ("CLEANGAB_WELCOME", "welcome.php");
 define ("CLEANGAB_404", "404.php");
 define ("CLEANGAB_XHTML_TEMPLATE", "base.xhtml");
 
-define ("CLEANGAB_APP_ENV", "DEV");
-define ("CLEANGAB_STACKTRACEDEBUG_FILE", "C:/Disys/log/cleangab.log");
-//define ("CLEANGAB_URL_BASE_APP", "http://localhost:800/portal");
-
-
-// Database connection
-// See localconfig.php from your application
-//define ("CLEANGAB_DB_HOST", "localhost");
-//define ("CLEANGAB_DB_PORT", "localhost");
-//define ("CLEANGAB_DB_USER", "root");
-//define ("CLEANGAB_DB_PWD", "");
-//define ("CLEANGAB_DB_DB", "cleangab");
+require ("localconfig.php");
 
 // Set the new Paths
 
