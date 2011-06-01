114d064e6f94816245fb50c87b518e72c7a7a12a
Leopoldo Barreiro <leo@cielnews.com>
2011-06-01 09:23:48 -0300

    Versão inicial do UserModel.

---
+++ core/model/UserModel.php
@@ -0,0 +1,17 @@
+<?php
+
+class UserModel extends CleanGabModel {
+
+	public function __construct() {
+		parent::__construct();
+		$this->listableFields = array("id", "user", "name", "email", "created");
+		$this->masks = array("created"=>"DateTimeFormatter");
+		$this->hintFields = array("id"=>"Id", "user"=>"User", "name"=>"Nome", "email"=>"e-mail", "created"=>"Data Cadastro");		
+		$entity = new Entity("user");
+		CleanGab::debug($entity);
+		$entity->init();
+		$this->recordset = $entity->retrieve();
+		$this->recordset->goFirst();
+	}
+}
+?>
