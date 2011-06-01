3a569c5a34eb364d7b3b81831ae9860bc85c4c9c
Leopoldo Barreiro <leo@cielnews.com>
2011-06-01 17:35:13 -0300

    Localconfig de exemplo.

---
+++ reference/localconfig.php
@@ -0,0 +1,13 @@
+<?php
+define ("CLEANGAB_DB_DRIVER", "mysql");
+define ("CLEANGAB_DB_HOST", "localhost");
+define ("CLEANGAB_DB_PORT", "3306");
+define ("CLEANGAB_DB_USER", "root");
+define ("CLEANGAB_DB_PWD", "");
+define ("CLEANGAB_DB_DB", "portal");
+define ("CLEANGAB_URL_BASE_APP", "http://localhost:800/portal-br");
+
+define ("CLEANGAB_APP_ENV", "DEV");
+define ("CLEANGAB_STACKTRACEDEBUG_FILE", "log" . DIRECTORY_SEPARATOR . "cleangab.log");
+
+?>
