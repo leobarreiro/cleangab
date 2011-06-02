ef9d219312732091b95c048f242cc68605d954e8
Leopoldo Barreiro <leo@cielnews.com>
2011-06-02 17:29:34 -0300

    Criada constante para controle de limites de paginação.

--- reference/localconfig.php
+++ reference/localconfig.php
@@ -6,8 +6,8 @@ define ("CLEANGAB_DB_USER", "root");
 define ("CLEANGAB_DB_PWD", "");
 define ("CLEANGAB_DB_DB", "portal");
 define ("CLEANGAB_URL_BASE_APP", "http://localhost:800/portal-br");
+define ("CLEANGAB_DEFAULT_SQL_LIMIT", "10");
 
 define ("CLEANGAB_APP_ENV", "DEV");
 define ("CLEANGAB_STACKTRACEDEBUG_FILE", "log" . DIRECTORY_SEPARATOR . "cleangab.log");
-
 ?>
