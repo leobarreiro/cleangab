f06eead5add0744c0e25658327a57c16b16599a7
Leopoldo Barreiro <leo@cielnews.com>
2011-06-03 17:34:13 -0300

    Suporte a debug em arquivo de log. Retirado debug em tela.

--- core/CleanGab.php
+++ core/CleanGab.php
@@ -3,19 +3,19 @@
 class CleanGab {
 	
 	static function debug($mixedVar) {
-		if (ini_get("error_reporting") > 0) {
-			echo "<pre>\n";
-			print_r($mixedVar);
-			echo "</pre>\n";
+		$file = (defined("CLEANGAB_STACKTRACEDEBUG_FILE")) ? CLEANGAB_STACKTRACEDEBUG_FILE : "log" . SEPARATOR . "cleangab.log";
+		$handle = (is_writable($file)) ? fopen($file, "a") : false;
+		if ($handle) {
+			fwrite($handle, "\n[" . date("Y.m.d H:i:s") . "] CG-" . strtoupper(CLEANGAB_APP_ENV) . " ");
+			fwrite($handle, "\t" . serialize($mixedVar));
 		}
 	}
 	
 	static function stackTraceDebug($exception) {
-		
 		$file = (defined("CLEANGAB_STACKTRACEDEBUG_FILE")) ? CLEANGAB_STACKTRACEDEBUG_FILE : "log" . SEPARATOR . "cleangab.log";
 		$handle = (is_writable($file)) ? fopen($file, "a") : false;
 		if ($handle) {
-			fwrite($handle, "\n\n[" . date("Y.m.d H:i:s") . "] CG-" . strtoupper(CLEANGAB_APP_ENV) . " ");
+			fwrite($handle, "\n[" . date("Y.m.d H:i:s") . "] CG-" . strtoupper(CLEANGAB_APP_ENV) . " ");
 			switch (strtoupper(CLEANGAB_APP_ENV)) {
 				case "DEV":
 					fwrite($handle, " msg: {" . $exception->getMessage() . "}, at:");
