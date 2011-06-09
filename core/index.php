ef957fc4ec37408a89fa0dc91d041a2bb0a01f3f
leopoldo.barreiro <leopoldo.barreiro@disys.com>
2011-06-09 17:28:08 -0300

    Identação corrigida.

--- core/index.php
+++ core/index.php
@@ -1,43 +1,56 @@
 <?php
-if (isset($_SERVER['SCRIPT_NAME']) && 
-	(preg_match("/\/cleangab\/core\/index.php/", $_SERVER['SCRIPT_NAME']) > 0)) {
+if (isset($_SERVER['SCRIPT_NAME']) && (preg_match("/\/cleangab\/core\/index.php/", $_SERVER['SCRIPT_NAME']) > 0)) 
+{
 	exit();
 }
 
-$friendlyUrl = array();
-$friendlyUrl['cgController'] 	= filter_input(INPUT_GET, "cgController", FILTER_SANITIZE_STRING);
-$friendlyUrl['cgAction'] 		= filter_input(INPUT_GET, "cgAction", FILTER_SANITIZE_STRING);
-$friendlyUrl['cgParam'] 		= filter_input(INPUT_GET, "cgParam", FILTER_SANITIZE_STRING);
+$friendlyUrl 				 = array();
+$friendlyUrl['cgController'] = filter_input(INPUT_GET, "cgController", FILTER_SANITIZE_STRING);
+$friendlyUrl['cgAction'] 	 = filter_input(INPUT_GET, "cgAction", FILTER_SANITIZE_STRING);
+$friendlyUrl['cgParam'] 	 = filter_input(INPUT_GET, "cgParam", FILTER_SANITIZE_STRING);
 
-try {
-	if ($friendlyUrl['cgController'] != null && strlen($friendlyUrl['cgController']) > 0) {
-		$controllerClass = ucfirst($_GET['cgController']) . "Controller";
-		if (in_array($controllerClass, get_declared_classes())) {
-			$controller = new $controllerClass;
-		} else {
-			include (CLEANGAB_404);
-			die();
-		}
-	} else {
-			include (CLEANGAB_WELCOME);
-			die();
+// Controller
+
+if ($friendlyUrl['cgController'] != null && 
+	strlen($friendlyUrl['cgController']) > 0) 
+{
+	$controllerClass = ucfirst($_GET['cgController']) . "Controller";
+	if (in_array($controllerClass, get_declared_classes())) 
+	{
+		$controller = new $controllerClass;
+	} 
+	else 
+	{
+		include (CLEANGAB_404);
+		die();
 	}
+} 
+else 
+{
+	include (CLEANGAB_WELCOME);
+	die();
+}
+
+// Action
+
+if ($friendlyUrl['cgAction'] != null && 
+	strlen($friendlyUrl['cgAction']) > 0 && 
+	method_exists($controller, $friendlyUrl['cgAction'])) 
+{
 	
-	if ($friendlyUrl['cgAction'] != null && strlen($friendlyUrl['cgAction']) > 0) {
-		if (method_exists($controller, $friendlyUrl['cgAction'])) {
-			if ($friendlyUrl['cgParam'] != null) {
-				$controller->$friendlyUrl['cgAction']($friendlyUrl['cgParam']);
-			} else {
-				$controller->$friendlyUrl['cgAction']();
-			}
-		} else {
-			include (CLEANGAB_404);
-			die();
-		}
-	} else {
-		$controller->index();
+	// Params
+	
+	if ($friendlyUrl['cgParam'] != null) 
+	{
+		$controller->$friendlyUrl['cgAction']($friendlyUrl['cgParam']);
+	} 
+	else 
+	{
+		$controller->{$friendlyUrl['cgAction']}();
 	}
-} catch (Exception $e) {
-	echo $e->getMessage();
+}
+else 
+{
+	$controller->index();
 }
 ?>
