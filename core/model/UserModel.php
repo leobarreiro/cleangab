68d7e6797ac69f4d81bc7d65b439016c2193de33
leopoldo.barreiro <leopoldo.barreiro@disys.com>
2011-06-10 17:20:47 -0300

    Aprimorado método prepareList().

--- core/model/UserModel.php
+++ core/model/UserModel.php
@@ -1,5 +1,4 @@
 <?php
-
 class UserModel extends CleanGabModel {
 
 	public function __construct() {
@@ -9,18 +8,38 @@ class UserModel extends CleanGabModel {
 		$this->hintFields = array("id"=>"Id", "user"=>"User", "name"=>"Nome", "email"=>"e-mail", "created"=>"Data Cadastro");		
 	}
 	
-	public function prepareList() {
-		$entity = new Entity("user");
-		$entity->init();
-		// argumentos
+	public function prepareList() 
+	{
+		$recoveredEntity = Session::getHostedObject("user-entity");
+		if ($recoveredEntity)
+		{
+			$this->entity = $recoveredEntity;
+		}
+		else 
+		{
+			$this->entity = new Entity("user");
+			$this->entity->init();
+		}
+
+		// Arguments
+		
 		if ($this->getArgumentData("user")) {
-			$entity->addArgument("user", $this->getArgumentData("user"), "LIKE");
+			$this->entity->addArgument("user", $this->getArgumentData("user"), "LIKE");
+		}
+		if ($this->getArgumentData("name")) {
+			$this->entity->addArgument("name", $this->getArgumentData("name"));
+		}
+		// pagination
+		if ($this->getArgumentData("pg"))
+		{
+			$this->entity->setOffset(($this->getArgumentData("pg")*$this->entity->getLimit())+1);
 		}
 		// order
 		if ($this->getArgumentData("order")) {
-			$entity->setOrderBy($this->getArgumentData("order"));
+			$this->entity->setOrderBy($this->getArgumentData("order"));
 		}
-		$this->setRecordset($entity->retrieve());
+		$this->setRecordset($this->entity->retrieve());
+		Session::hostObject("user-entity", $this->entity);
 	}
 	
 }
