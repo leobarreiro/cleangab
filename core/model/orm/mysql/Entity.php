0dc3c6c85ec11bb19505bc89812d4cb09c8b1a30
leopoldo.barreiro <leopoldo.barreiro@disys.com>
2011-06-10 17:16:58 -0300

    Criado método para conexão de retaguarda. Atributos sql para antes e depois do parser.

--- core/model/orm/mysql/Entity.php
+++ core/model/orm/mysql/Entity.php
@@ -19,6 +19,8 @@ class Entity implements IDBEntity {
 	private $offset;
 	private $limit;
 	private $total;
+	private $sqlBeforeParser;
+	private $sqlAfterParser;
 	private $pk;
 	private $fk;
 	private $uk;
@@ -27,19 +29,13 @@ class Entity implements IDBEntity {
 	private $stringTypes;
 	private $numericTypes;
 	
-	public function __construct($tableName, $dataBase=CLEANGAB_DB_DB, $connection=null)
+	public function __construct($tableName, $dataBase=CLEANGAB_DB_DB)
 	{
-		if ($connection != null) {
-			$this->setConnection($connection);
-		} else {
-			$this->connection = new Connection();
-		}
+		$this->connection = null;
 		$this->stringTypes  = unserialize(CLEANGAB_SQL_STRING_TYPES);
 		$this->numericTypes = unserialize(CLEANGAB_SQL_NUMERIC_TYPES);
-		
 		$this->setDataBase($dataBase);
 		$this->setTableName($tableName);
-
 		$this->fields 	= array();
 		$this->pk 		= array();
 		$this->fk 		= array();
@@ -48,34 +44,66 @@ class Entity implements IDBEntity {
 		$this->args 	= array();
 		$this->offset 	= 0;
 		$this->limit  	= CLEANGAB_DEFAULT_SQL_LIMIT;
+		$this->sqlBeforeParser = null;
+		$this->sqlAfterParser = null;
 	}
 	
-	public function setConnection($conn) {
+	private function connectIfNull() 
+	{
+		if ($this->connection == null)
+		{
+			$this->connection = new Connection();
+		}
+	}
+	
+	public function setConnection($conn) 
+	{
 		if (is_object($conn) && get_class($conn) == "Connection") {
 			$this->connection = $conn;
 		}
 	}
 	
-	public function addArgument($key, $search, $operation='LIKE') {
-		if (array_key_exists($key, $this->fields)) {
+	public function addArgument($key, $search, $operation='LIKE') 
+	{
+		if (array_key_exists($key, $this->fields)) 
+		{
 			$this->args[] = array('key'=>$key, 'search'=>$search, 'operation'=>$operation);
 		}
 	}
 	
-	public function setOrderBy($strOrder) {
+	public function setOrderBy($strOrder) 
+	{
 		$this->orderBy = $strOrder;
 	}
 
-	public function setOffset($intOffset) {
+	public function setOffset($intOffset) 
+	{
 		$this->offset = $intOffset;
 	}
 	
-	public function setLimit($intLimit) {
+	public function setLimit($intLimit) 
+	{
 		$this->limit = $intLimit;
 	}
 	
-	public function getConnection() {
+	public function getConnection() 
+	{
 		return $this->connection;
+	}
+	
+	public function getOffset() 
+	{
+		return $this->offset;
+	}
+	
+	public function getLimit() 
+	{
+		return $this->limit;
+	}
+	
+	public function getTotal() 
+	{
+		return $this->total;
 	}
 	
 	public function init()
@@ -84,9 +112,12 @@ class Entity implements IDBEntity {
 		$primaryKeys = array();
 		$foreignKeys = array();
 		$uniqueKeys  = array();
-		try {
+		$this->connectIfNull();
+		try 
+		{
 			$qr = $this->connection->resource->query($this->prepare(CLEANGAB_SQL_RETRIEVE_TABLE_FIELDS));
-			if (!$qr) {
+			if (!$qr) 
+			{
 				throw new Exception("Error when retrieving table fields", "3");
 			}
 			while ($record = $qr->fetch_object())
@@ -109,18 +140,23 @@ class Entity implements IDBEntity {
 												'extra'=>$record->Extra, 
 												'default'=>$record->Default, 
 												'value'=>'');
-				if ($record->Key == 'PRI') {
+				if ($record->Key == 'PRI') 
+				{
 					$primaryKeys[] = $record->Field;
 				}
-				if ($record->Key == 'MUL') {
+				if ($record->Key == 'MUL') 
+				{
 					$foreignKeys[] = $record->Field;
 				}
-				if ($record->Key == 'UNI') {
+				if ($record->Key == 'UNI') 
+				{
 					$uniqueKeys[] = $record->Field;
 				}
 			}
 			$qr->close();
-		} catch (Exception $e) {
+		} 
+		catch (Exception $e) 
+		{
 			CleanGab::stackTraceDebug($e);
 		}
 		$this->fields  = $fields;
@@ -215,7 +251,9 @@ class Entity implements IDBEntity {
 	}
 	
 	public function retrieve($sql=CLEANGAB_SQL_RETRIEVE_ALL) {
-		$qr = $this->connection->resource->query($this->prepare($sql));
+		$this->connectIfNull();
+		$this->sqlBeforeParser = $sql;
+		$qr = $this->connection->resource->query($this->prepare($this->sqlBeforeParser));
 		$recordset = new Recordset($qr);
 		$this->countRecords();
 		return $recordset;
@@ -228,20 +266,23 @@ class Entity implements IDBEntity {
 		$this->total = $record['total'];
 	}
 	
-	private function prepare($sql) {
+	private function prepare($sql) 
+	{
 		$old = array("[database]", "[table]");
 		$new = array($this->dataBase, $this->tableName);
 		
 		$old[] = "[pk]";
 		$new[] = implode (", ", $this->pk);
 		
-		if ($this->fields != null) {
+		if ($this->fields != null) 
+		{
 			$old[] = "[listable_fields]";
 			$new[] = implode(", ", array_keys($this->fields));
 		}
 		
 		$sqlArguments = array();
-		foreach ($this->args as $arg) {
+		foreach ($this->args as $arg) 
+		{
 			$sqlArguments[] = $this->parseArgumentToSql($arg);
 		}
 
@@ -255,18 +296,26 @@ class Entity implements IDBEntity {
 		$new[] = "LIMIT " . $this->offset . ", " . $this->limit;
 		
 		$sql = str_replace($old, $new, $sql);
+		$this->sqlAfterParser = $sql;
 		return $sql;
 	}
 	
-	private function parseArgumentToSql($arArgument) {
+	private function parseArgumentToSql($arArgument) 
+	{
 		$sqlPart  = $arArgument['key'];
 		$sqlPart .= " " . $arArgument['operation'] . " ";
-		if (in_array($this->fields[$arArgument['key']]['type'], $this->numericTypes)) {
+		if (in_array($this->fields[$arArgument['key']]['type'], $this->numericTypes)) 
+		{
 			$sqlPart .= $arArgument['search'];
-		} else {
-			if ($arArgument['operation'] == "LIKE") {
+		} 
+		else 
+		{
+			if ($arArgument['operation'] == "LIKE") 
+			{
 				$sqlPart .= "'%" . $arArgument['search'] . "%'";
-			} else {
+			} 
+			else 
+			{
 				$sqlPart .= "'" . $arArgument['search'] . "'";
 			}
 		}
