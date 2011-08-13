a12cc99ab5eaf091451432d8af9cd3f18191d292
Leopoldo Barreiro <leo@e-faber.com.br>
2011-08-13 13:08:58 -0300

    Versão inicial das classes de componente xhtml Input e SelectInput.

---
+++ core/view/SelectInput.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * CleanGab Framework
+ * SelectInput.php
+ * Date: 	2011-08-03
+ * Author: 	Leopoldo Barreiro
+ */
+
+require_once ("Input.php");
+
+class SelectInput extends Input {
+	
+	protected $options;
+	
+	public function __construct($name, $value, $options=array()) 
+	{
+		parent::__construct($name, $value, "select");
+		$this->options = $options;
+	}
+	
+	public function addOption($key, $value)
+	{
+		$this->options[$key] = $value;
+	}
+	
+	public function setOptions($arOptions)
+	{
+		$this->options = $arOptions;
+	}
+	
+	public function ensamble() 
+	{
+		$xhtml = array();
+		$xhtml[] = "<select";
+		$xhtml[] = " name=\"" . $this->name . "\"";
+		$xhtml[] = " id=\"" . $this->id . "\"";
+		if (strlen($this->cssClass) > 0) 
+		{
+			$xhtml[] = " class=\"" . $this->cssClass . "\"";
+		}
+		foreach ($this->attribs as $attrName=>$attrValue)
+		{
+			$xhtml[] = " " . $attrName . "=\"" . $attrValue ."\"";
+		}
+		foreach ($this->triggersJs as $event=>$todo)
+		{
+			$xhtml[] = " " . $event . "=\"" . $todo ."\"";
+		}
+		$xhtml[] = ">";
+		
+		foreach ($this->options as $opt)
+		{
+			$option = array();
+			$option[] = "<option value=\"" . $opt->k . "\"";
+			if ($this->value == $opt->v)
+			{
+				$option[] = " selected";
+			}
+			$option[] = ">";
+			$option[] = $opt->v . "</option>";
+			$xhtml[] = implode("", $option);
+		}
+		
+		$xhtml[] = "</select>";
+		$this->xhtml = implode("", $xhtml);
+	}
+	
+}
+?>
