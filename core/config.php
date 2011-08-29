f48154d16674d168c529d9c5c0f2dabfcf9068c1
leobarreiro <leo@e-faber.com.br>
2011-08-29 09:39:38 -0300

    Retirado include localconfig.

--- core/config.php
+++ core/config.php
@@ -1,21 +1,21 @@
 <?php
 /**
- * Clean-Gab Framework
+ * CleanGab Framework
+ * http://github.com/leobarreiro/cleangab/
  * config.php
  * Date: 	2011-01-27
  * Author: 	Leopoldo Barreiro
  */
 // Constants
 
-define ("CLEANGAB_FWK_ROOT", "C:\\Dev\\apps\\apache\\htdocs\\cleangab\\core");
+//define ("CLEANGAB_FWK_ROOT", "C:\\Dev\\apps\\apache\\htdocs\\cleangab\\core");
+define ("CLEANGAB_FWK_ROOT", "/var/www/cleangab/core");
 define ("CLEANGAB_FWK_MODEL", CLEANGAB_FWK_ROOT . DIRECTORY_SEPARATOR . "model");
 define ("CLEANGAB_FWK_ORM", CLEANGAB_FWK_MODEL . DIRECTORY_SEPARATOR . "orm");
 define ("CLEANGAB_FWK_CONTROL", CLEANGAB_FWK_ROOT . DIRECTORY_SEPARATOR . "controller");
 define ("CLEANGAB_FWK_VIEW", CLEANGAB_FWK_ROOT . DIRECTORY_SEPARATOR . "view");
 define ("CLEANGAB_FWK_SECURITY", CLEANGAB_FWK_ROOT . DIRECTORY_SEPARATOR . "security");
 define ("CLEANGAB_FWK_FORMATTER", CLEANGAB_FWK_VIEW . DIRECTORY_SEPARATOR . "formatters");
-
-require ("localconfig.php");
 
 define ("CLEANGAB_WELCOME", "welcome.php");
 define ("CLEANGAB_404", "404.php");
