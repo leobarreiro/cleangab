075006bc1ba11a34cc4ad63737c2107c581e928d
Leopoldo Barreiro <leo@e-faber.com.br>
2011-08-31 23:10:38 -0300

    Alterado nome de constante do arquivo de Log. Utilização de constante mágica __DIR__ para definição de paths.

--- reference/localconfig.php
+++ reference/localconfig.php
@@ -6,9 +6,9 @@ define ("CLEANGAB_DB_USER", "root");
 define ("CLEANGAB_DB_PWD", "");
 define ("CLEANGAB_DB_DB", "portal");
 define ("CLEANGAB_URL_BASE_APP", "http://localhost:800/portal-br");
-define ("CLEANGAB_PATH_BASE_APP", "/var/www/portal-br");
+define ("CLEANGAB_PATH_BASE_APP", __DIR__);
 define ("CLEANGAB_DEFAULT_SQL_LIMIT", "10");
-define ("CLEANGAB_STACKTRACEDEBUG_FILE", "C:\\Dev\\projetos\\portal-br\\log\\cleangab.log");
+define ("CLEANGAB_LOG_FILE", __DIR__ . DIRECTORY_SEPARATOR . "log" . DIRECTORY_SEPARATOR . "cleangab.log");
 define ("CLEANGAB_APP_ENV", "DEV");
 define ("CLEANGAB_APP_INTERFACE_TYPE", "CLI"); // Valores possiveis: CLI (PHP CLI), WEB (PHP Web).
 define ("CLEANGAB_EMAIL_PARAMS", serialize(array("email_sender"=>"ononono@onono.com", "auth"=>true, "host"=>"ssl://smtp.gmail.com", "username"=>"ononono@onono.com", "password"=>"123123123", "port"=>"465")));
