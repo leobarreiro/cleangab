893caab8e993409c51fe59a2ee8887719b741430
Leopoldo Barreiro <leo@e-faber.com.br>
2012-01-17 22:34:09 -0200

    Alterada posição do parâmetro APP_LANGUAGE

--- reference/localconfig.php
+++ reference/localconfig.php
@@ -10,11 +10,11 @@ define ("CLEANGAB_PATH_BASE_APP", dirname(__FILE__));
 define ("CLEANGAB_DEFAULT_SQL_LIMIT", "10");
 define ("CLEANGAB_LOG_FILE", dirname(__FILE__) . DIRECTORY_SEPARATOR . "log" . DIRECTORY_SEPARATOR . "cleangab.log");
 define ("CLEANGAB_APP_ENV", "DEV");
+define ("CLEANGAB_APP_LANGUAGE", "pt_BR");
 define ("CLEANGAB_APP_INTERFACE_TYPE", "CLI"); // Valores possiveis: CLI (PHP CLI), WEB (PHP Web).
 define ("CLEANGAB_EMAIL_PARAMS", serialize(array("email_sender"=>"ononono@onono.com", "auth"=>true, "host"=>"ssl://smtp.gmail.com", "username"=>"ononono@onono.com", "password"=>"123123123", "port"=>"465")));
 define ("CLEANGAB_AUTH_METHOD", "DB"); // Microsoft Active Directory: AD; DataBase Application: DB
 define ("CLEANGAB_AD_HOST", "lanpoap01");
 define ("CLEANGAB_AD_DOMAIN", "disys");
 define ("CLEANGAB_AD_PORT", "");
-define ("CLEANGAB_APP_LANGUAGE", "pt_BR");
 ?>
